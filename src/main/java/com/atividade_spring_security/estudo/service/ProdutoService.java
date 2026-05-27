6a9ff3977da8ba2fbe27323530f3d9c7d0335173
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-05-27 19:48:27 -0300

    service

---
+++ src/main/java/com/atividade_spring_security/estudo/service/ProdutoService.java
@@ -0,0 +1,26 @@
+package com.atividade_spring_security.estudo.service;
+
+import com.atividade_spring_security.estudo.model.ProdutoModel;
+import com.atividade_spring_security.estudo.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private ProdutoRepository repository;
+
+    public List<ProdutoModel>listarTodos(){
+        return repository.findAll();
+    }
+
+    public ProdutoModel save(ProdutoModel produtoModel){
+        return repository.save(produtoModel);
+    }
+
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+}
